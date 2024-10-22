63f72451dd9f4a2d80fe66460d00c3e4bfe7f401
ekgns33 <ekgns00421@gmail.com>
2024-10-22 13:33:40 +0900

    chore : update workflow

--- server/src/main/java/org/kunp/Main.java
+++ server/src/main/java/org/kunp/Main.java
@@ -1,7 +1,23 @@
 package org.kunp;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("Hello world!");
+
+    //example codes
+    ServerSocket serverSocket = new ServerSocket(8080);
+    while (true) {
+      Socket accept = serverSocket.accept();
+
+      System.out.println("Accepted connection from " + accept.getInetAddress());
+      accept.close();
+      break;
+    }
+    serverSocket.close();
+    return;
   }
 }
