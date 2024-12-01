281deff2b95345fcbdbb763027d58cfb9bc8308c
zziglet <jjw04033@konkuk.ac.kr>
2024-12-01 19:34:31 +0900

    fix: 대기실 명단 오류 수정

--- client/src/main/java/org/kunp/inner/InnerWaitingRoomComponent.java
+++ client/src/main/java/org/kunp/inner/InnerWaitingRoomComponent.java
@@ -11,6 +11,7 @@ import java.util.Set;
 
 public class InnerWaitingRoomComponent extends JPanel {
   public InnerWaitingRoomComponent(StateManager stateManager, ServerCommunicator serverCommunicator, String roomName) {
+    System.out.println("move inner");
     setLayout(new BorderLayout());
     setBorder(BorderFactory.createLineBorder(Color.GRAY));
     setPreferredSize(new Dimension(350, 200));
@@ -28,45 +29,42 @@ public class InnerWaitingRoomComponent extends JPanel {
 
     // 메시지 리스너 등록
     ServerCommunicator.ServerMessageListener listener = message -> {
-      if (stateManager.getCurrentScreen().equals("InnerWaitingRoom")) {
-        handleServerMessage(stateManager, message, sessionIds, listPanel);
-      }
+      System.out.println("inner message");
+      handleServerMessage(stateManager, message, sessionIds, listPanel);
     };
 
     stateManager.addMessageListener(listener);
-
-    // 컴포넌트가 비활성화될 때 리스너 제거
-    addComponentListener(new java.awt.event.ComponentAdapter() {
-      @Override
-      public void componentHidden(java.awt.event.ComponentEvent e) {
-        stateManager.removeMessageListener(listener);
-      }
-    });
   }
 
   private void handleServerMessage(StateManager stateManager, String message, Set<String> sessionIds, InnerWaitingRoomListPanel listPanel) {
     String[] tokens = message.split("\\|");
+
     if (tokens.length > 1) {
       String type = tokens[0];
-      String[] data = tokens[1].split(",");
-
-      switch (type) {
-        case "110": // 새 사용자 입장
-          sessionIds.addAll(Set.of(data));
-          break;
-        case "111": // 사용자 퇴장
-          sessionIds.remove(data[0]);
-          break;
-        case "113": // 게임 시작
-          stateManager.switchTo("Map");
-          break;
-        default:
-          System.out.println("Unhandled message type: " + type);
+      System.out.println(type);
+      if (type.equals("110")) {
+        String[] sessions = message.split("\n");
+        String lastSessionMessage = sessions[sessions.length - 1];
+        String[] sessionData = lastSessionMessage.split("\\|");
+        if (sessionData.length > 1) {
+          sessionIds.clear();
+          String[] sessionIdsArray = sessionData[1].split(",");
+          sessionIds.addAll(Set.of(sessionIdsArray));
+        }
+      }
+      else if (type.equals("111")) { // 사용자 퇴장
+        String[] data = tokens[1].split(",");
+        sessionIds.remove(data[0]);
+      } else if (type.equals("113")) { // 게임 시작
+        stateManager.switchTo("Map");
+      } else {
+        System.out.println("Unhandled message type: " + type);
       }
 
       SwingUtilities.invokeLater(() -> listPanel.updateSessionList(sessionIds));
     }
   }
+
 }
 
 
